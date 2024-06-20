8f2c4679f2bd7d68e3ce7301632078e660dfd2c5
vic <74506415+v1ctorio@users.noreply.github.com>
2024-06-20 10:24:47 +0200

    Add neovim configuration (nvchad) (idk if i can do this sorry)

---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre' -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  {
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  }
+  -- These are some examples, uncomment them if you want to see them work!
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   config = function()
+  --     require("nvchad.configs.lspconfig").defaults()
+  --     require "configs.lspconfig"
+  --   end,
+  -- },
+
+  -- {
+  -- 	"williamboman/mason.nvim",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"lua-language-server", "stylua",
+  -- 			"html-lsp", "css-lsp" , "prettier"
+  -- 		},
+  -- 	},
+  -- },
+  --
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
