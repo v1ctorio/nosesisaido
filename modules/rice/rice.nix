06903bad1bcb4552e86095381ee5f037a7745bc9
vic <74506415+v1ctorio@users.noreply.github.com>
2025-01-02 13:05:50 +0100

    add ags symlink

--- modules/rice/rice.nix
+++ modules/rice/rice.nix
@@ -6,6 +6,9 @@ in
   home.file.".config/tofi/".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/modules/rice/tofi";
   home.file.".config/waybar/".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/modules/rice/waybar";
   home.file.".config/wofi/".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/modules/rice/wofi";
+  
+  home.file.".config/ags/".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/modules/rice/ags";
+  
   programs.waybar.enable = true;
   programs.wofi.enable = true;
   programs.tofi.enable = true;
